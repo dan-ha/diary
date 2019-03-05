471801c272050a47e64805aecadb5d8e241988e8
danha <daniel.hamarik@gmail.com>
2019-03-05 10:09:29 +0100

    view diary entry

--- routes/entries.js
+++ routes/entries.js
@@ -13,4 +13,27 @@ router.get('/add', (req, res, next) => {
     });
 });
 
+// Save Diary Entry
+router.post('/save', async (req, res, next) => {
+    var diaryEntry;
+    if (req.body.docreate === 'create') {
+        diaryEntry = await entries.create(req.body.date,
+            req.body.title, req.body.content);
+    } else {
+        diaryEntry = await entries.update(req.body.date,
+            req.body.title, req.body.content);
+    }
+    res.redirect('/entry/view?date=' + req.body.date);
+});
+
+// Read Diary Entry
+router.get('/view', async (req, res, next) => {
+    var diaryEntry = await entries.read(req.query.date);
+    res.render('entryview', {
+        title: diaryEntry ? diaryEntry.title : "",
+        date: req.query.date,
+        entry: diaryEntry
+    });
+});
+
 module.exports = router;
