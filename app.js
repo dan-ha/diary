f3db6449b7c123239ad32d4cdd6dfbaca765e3d0
danha <daniel.hamarik@gmail.com>
2019-03-06 08:48:41 +0100

    setup logging with  rotating-file-stream

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
+const fs = require('fs-extra');
 const hbs = require('hbs');
+
+// Logging dependencies
+var logger = require('morgan');
+const rfs = require('rotating-file-stream');
 
 var indexRouter = require('./routes/index');
 var entryRouter = require('./routes/entries');
@@ -16,7 +20,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 
-app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,6 +27,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/entry', entryRouter);
+
+// Setup logging
+var logStream;
+// Log to a file if requested (with rotaing-file-stream)
+if (process.env.REQUEST_LOG_FILE) {
+  (async() => {
+    let logDirectory = path.dirname(process.env.REQUEST_LOG_FILE);
+    await fs.ensureDir(logDirectory);
+    logStream = rfs(process.env.REQUEST_LOG_FILE, {
+      size: '10M',      // rotate every 10 MegaBytes written
+      interval: '1d',   // rotate daily
+      compress: 'gzip'  // compress rotated files
+    });
+  })().catch(err => { console.error(err); });
+}
+app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev', {
+  stream: logStream ? logStream : process.stdout
+}));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
