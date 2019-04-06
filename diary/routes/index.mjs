d7f59575281bce223bd1834e7fcdecced4fa9ad2
danha <daniel.hamarik@gmail.com>
2019-04-06 18:39:03 +0200

    style history sidebar

--- diary/routes/index.mjs
+++ diary/routes/index.mjs
@@ -8,13 +8,18 @@ export const router = express.Router();
 
 /* GET home page. */
 router.get('/', ensureAuthenticated, async (req, res, next) => {
+  const today = new Date().setHours(0,0,0,0);
   let diaryEntries = await entries.findAllEntries(req.user.username);
   const dates = diaryEntries.map((entry) => {
-    return moment(entry.date).format("Do MMM YY");
+    const formattedDate = moment(entry.date).format("Do MMM YY"); 
+    return {
+      date: entry.date, 
+      formattedDate: formattedDate
+    };
   });
-  console.log(dates);
   res.render('index', { 
     title: 'Online Diary',
     user: req.user ? req.user : undefined,
+    today: today,
     entryDates: dates });
 });
