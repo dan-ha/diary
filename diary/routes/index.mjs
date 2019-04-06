9b3767d270c90a001f1b17b86db7400e5d2cf38d
danha <daniel.hamarik@gmail.com>
2019-04-06 14:35:06 +0200

    format entry dates with moment

--- diary/routes/index.mjs
+++ diary/routes/index.mjs
@@ -1,4 +1,5 @@
 import util from 'util';
+import moment from 'moment';
 import express from 'express';
 import * as entries from '../models/entries';
 import { ensureAuthenticated } from './users';
@@ -8,8 +9,12 @@ export const router = express.Router();
 /* GET home page. */
 router.get('/', ensureAuthenticated, async (req, res, next) => {
   let diaryEntries = await entries.findAllEntries(req.user.username);
+  const dates = diaryEntries.map((entry) => {
+    return moment(entry.date).format("Do MMM YY");
+  });
+  console.log(dates);
   res.render('index', { 
     title: 'Online Diary',
     user: req.user ? req.user : undefined,
-    diaryEntries: diaryEntries });
+    entryDates: dates });
 });
