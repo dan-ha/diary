081839e20a4fe30bc4afb5ec6df9b75b95c5456f
danha <daniel.hamarik@gmail.com>
2019-05-19 21:33:40 +0200

    heroku cd-configuration

--- user-service/src/utils/apiKeyAuthentication.mjs
+++ user-service/src/utils/apiKeyAuthentication.mjs
@@ -2,7 +2,7 @@ import errors from 'restify-errors';
 import isEmpty from 'lodash/isEmpty';
 
 export function authenticateApiKey(req, res, next) {
-    if (!isEmpty(req.authorization)) {
+    if (!isEmpty(req.authorization.basic)) {
         if ((req.authorization.basic.username === process.env.AUTH_USERNAME)
          &&
           (req.authorization.basic.password === process.env.AUTH_PASSWORD)) {
